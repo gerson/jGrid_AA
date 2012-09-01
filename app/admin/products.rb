b5f45877262d23aaf6c95ace04168fb1c0c3dcfa
Gerson Villanueva <gerson.villanueva@koombea.com>
2012-09-01 17:13:33 -0500

    add activeadmin and jqGrid

---
+++ app/admin/products.rb
@@ -0,0 +1,8 @@
+ActiveAdmin.register_page "Products" do
+  menu :label => "Products"
+
+  content do
+    table :id => "list4"
+    div :id => "pager4"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JGridAA::Application.config.secret_token = '9929187cf37785cc8ddaf67023eca3afafc39ba3ac2abbccde7123bfc7b892d748bdfc1f625f2fad6756f5167299fa4f0d22043d07412906df8e2a715227ead1'
